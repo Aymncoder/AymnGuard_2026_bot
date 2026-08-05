a56ac7d0052d62f7949e62af0df331b0b53ef3a7
Aymncoder <Aymnalsbyhy2001@gmail.com>
2026-08-06 02:24:07 +0300

    Fix import statement for Base model in db.py

--- database/db.py
+++ database/db.py
@@ -17,6 +17,7 @@ from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlalchemy.sql import text
 from database.models import Base
+from .models import Base
 
 # إعداد السجلات المؤسسية
 logger = logging.getLogger("AymnGuard.DatabaseEngine")
