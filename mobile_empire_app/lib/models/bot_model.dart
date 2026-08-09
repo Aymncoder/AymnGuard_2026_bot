0f8b72d55d10dfaa9fbdb058f28f39893562735e
Aymncoder <Aymnalsbyhy2001@gmail.com>
2026-08-09 04:12:08 +0300

    Update bot_model.dart

--- mobile_empire_app/lib/models/bot_model.dart
+++ mobile_empire_app/lib/models/bot_model.dart
@@ -1,4 +1,7 @@
-import 'package:flutter/material.dart';
+import 'package:mobile_empire_app/app_config.dart';
+import 'package:mobile_empire_app/bot_model.dart';
+import 'package:mobile_empire_app/api_service.dart';
+import 'package:mobile_empire_app/widgets/smart_contract_audit_widget.dart';
 
 // ==============================================================================
 // نموذج بيانات البوتات السيادية (Sovereign Bot Data Model)
