35335a6638c4231ca2a52ef875e8539b5d667ff6
Aymncoder <Aymnalsbyhy2001@gmail.com>
2026-08-09 18:06:05 +0300

    Fix import statement for app_config.dart

--- mobile_empire_app/lib/telegram_core_chats_screen.dart
+++ mobile_empire_app/lib/telegram_core_chats_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-export 'app_config.dart';
+import 'app_config.dart';
 import 'package:mobile_empire_app/chat_widgets.dart'; 
 import 'package:mobile_empire_app/community_widgets.dart'; 
 
