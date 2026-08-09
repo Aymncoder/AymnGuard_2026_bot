05c6f85bc28717d7db665ef09ba4a785f2d56c55
Aymncoder <Aymnalsbyhy2001@gmail.com>
2026-08-09 18:09:54 +0300

    Fix import statement for app_config.dart

--- mobile_empire_app/lib/community_widgets.dart
+++ mobile_empire_app/lib/community_widgets.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-export 'app_config.dart';
+import 'app_config.dart';
 
 // ==============================================================================
 // مكونات بوابة مجتمعاتي المستقلة (Community Portal Widgets)
