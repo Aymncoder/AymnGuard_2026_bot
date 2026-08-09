705a54032e479fcc0bdbb465570d18259de6747a
Aymncoder <Aymnalsbyhy2001@gmail.com>
2026-08-09 04:10:43 +0300

    Clean up imports in settings_screens.dart
    Removed unused import statements from settings_screens.dart.

--- mobile_empire_app/lib/lib/settings_screens.dart
+++ mobile_empire_app/lib/lib/settings_screens.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/material.dart';
-
-import 'app_config.dart';
+import 'package:mobile_empire_app/app_config.dart';
+import 'package:mobile_empire_app/bot_model.dart';
+import 'package:mobile_empire_app/api_service.dart';
+import 'package:mobile_empire_app/widgets/smart_contract_audit_widget.dart';
 
 // ==============================================================================
 // شاشات الإعدادات والخصوصية (Settings & Privacy Screens)
