2f73c74c678648a6a50b4df82a6e76c9026184b6
Aymncoder <Aymnalsbyhy2001@gmail.com>
2026-08-09 15:31:25 +0300

    Update telegram_core_chats_screen.dart

--- mobile_empire_app/lib/telegram_core_chats_screen.dart
+++ mobile_empire_app/lib/telegram_core_chats_screen.dart
@@ -1,14 +1,8 @@
-import 'package:mobile_empire_app/app_config.dart';
 import 'package:flutter/material.dart';
 
-import 'package:mobile_empire_app/models/bot_model.dart'; // مسار صحيح داخل مجلد models
-import 'package:mobile_empire_app/api_service.dart';
-import 'package:mobile_empire_app/widgets/smart_contract_audit_widget.dart'; // مسار صحيح داخل مجلد widgets
-import 'package:mobile_empire_app/telegram_core_chats_screen.dart';
-import 'package:mobile_empire_app/premium_dashboard_screens.dart';
-import 'package:mobile_empire_app/settings_screens.dart';
-import 'package:mobile_empire_app/app_drawers.dart';
-
+import 'package:mobile_empire_app/core/app_config.dart';
+import 'package:mobile_empire_app/chat_widgets.dart'; // لاستدعاء AllChatsListWidget
+import 'package:mobile_empire_app/community_widgets.dart'; // لاستدعاء MyCommunitiesPortalWidget
 
 // ==============================================================================
 // 5. نواة تليجرام المجانية المفتوحة (Telegram Core UI)
