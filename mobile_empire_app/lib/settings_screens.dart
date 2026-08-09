45e3f919a5a1b00d5ac8135b42c8985671cd3ec1
Aymncoder <Aymnalsbyhy2001@gmail.com>
2026-08-09 17:50:08 +0300

    Export app_config.dart instead of importing it

--- mobile_empire_app/lib/settings_screens.dart
+++ mobile_empire_app/lib/settings_screens.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mobile_empire_app/app_config.dart';
+export 'app_config.dart';
 
 // ==============================================================================
 // شاشات الإعدادات والخصوصية (Settings & Privacy Screens)
