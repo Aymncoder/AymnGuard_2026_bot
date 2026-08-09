232648cec919cc21707caf2db716187393a4fc46
Aymncoder <Aymnalsbyhy2001@gmail.com>
2026-08-09 18:17:15 +0300

    Update main.dart

--- mobile_empire_app/lib/main.dart
+++ mobile_empire_app/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+حimport 'package:flutter/material.dart';
 import 'app_config.dart';
 
 // مسارات الشاشات والقوائم المستخدمة في الواجهة الرئيسية
@@ -38,6 +38,19 @@ class AymnGuardPlusApp extends StatelessWidget {
       home: const MainSovereignScreen(),
     );
   }
+}
+class AppConfig {
+  static const String serverUrl = "http://135.181.86.199:10050";
+  static const String appVersion = "21.0.0 Enterprise";
+  static const bool isOwnerStatus = true;
+}
+
+class AppColors {
+  static const Color background = Color(0xFF151E27);
+  static const Color surface = Color(0xFF1E293B);
+  static const Color primary = Color(0xFF0EA5E9);
+  static const Color accentGold = Color(0xFFFFD700);
+  static const Color chatBackground = Color(0xFF0F172A);
 }
 
 // ==============================================================================
