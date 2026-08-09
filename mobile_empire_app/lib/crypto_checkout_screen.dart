3fd14e565f948d5e6887a5ff8f88437b8d823ceb
Aymncoder <Aymnalsbyhy2001@gmail.com>
2026-08-09 17:51:05 +0300

    Export app_config.dart in crypto_checkout_screen.dart

--- mobile_empire_app/lib/crypto_checkout_screen.dart
+++ mobile_empire_app/lib/crypto_checkout_screen.dart
@@ -1,8 +1,9 @@
 import 'dart:async';
+export 'app_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:qr_flutter/qr_flutter.dart';
-import 'package:mobile_empire_app/app_config.dart';
+
 
 class CryptoCheckoutScreen extends StatefulWidget {
   final String invoiceId;
