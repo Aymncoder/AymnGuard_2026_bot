c74b7ffb3024c2cf098a8a7147435960db869888
Aymncoder <Aymnalsbyhy2001@gmail.com>
2026-08-09 04:16:29 +0300

    Refactor imports in chat_room_screen.dart

--- mobile_empire_app/lib/chat_room_screen.dart
+++ mobile_empire_app/lib/chat_room_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'app_config.dart';
-import 'app_config.dart';
 import 'chat_widgets.dart';
+import 'package:mobile_empire_app/app_config.dart';
+import 'package:mobile_empire_app/bot_model.dart';
+import 'package:mobile_empire_app/api_service.dart';
+import 'package:mobile_empire_app/widgets/smart_contract_audit_widget.dart';
 
 // ==============================================================================
 // 6. شاشة المحادثة الفعلية (Chat Room UI)
