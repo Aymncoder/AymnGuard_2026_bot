2732e65b28bb48490c851e54b6f6068273a1530d
Aymncoder <Aymnalsbyhy2001@gmail.com>
2026-08-09 16:12:38 +0300

    Update app_config.dart

--- mobile_empire_app/lib/app_config.dart
+++ mobile_empire_app/lib/app_config.dart
@@ -1,1 +1,15 @@
-export 'core/app_config.dart';
+import 'package:flutter/material.dart';
+
+abstract class AppConfig {
+  static const String serverUrl = "http://135.181.86.199:10050";
+  static const String appVersion = "21.0.0 Enterprise";
+  static const bool isOwnerStatus = true;
+}
+
+abstract class AppColors {
+  static const Color background = Color(0xFF151E27);
+  static const Color surface = Color(0xFF1E293B);
+  static const Color primary = Color(0xFF0EA5E9);
+  static const Color accentGold = Color(0xFFFFD700);
+  static const Color chatBackground = Color(0xFF0F172A);
+}
