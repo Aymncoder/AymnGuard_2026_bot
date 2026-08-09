3660e3fda6275e4466600845901d1704247dd356
Aymncoder <Aymnalsbyhy2001@gmail.com>
2026-08-09 18:50:15 +0300

    Update main.dart

--- mobile_empire_app/lib/main.dart
+++ mobile_empire_app/lib/main.dart
@@ -40,20 +40,6 @@ class AymnGuardPlusApp extends StatelessWidget {
   }
 }
 
-class AppConfig {
-  static const String serverUrl = "http://135.181.86.199:10050";
-  static const String appVersion = "21.0.0 Enterprise";
-  static const bool isOwnerStatus = true;
-}
-
-class AppColors {
-  static const Color background = Color(0xFF151E27);
-  static const Color surface = Color(0xFF1E293B);
-  static const Color primary = Color(0xFF0EA5E9);
-  static const Color accentGold = Color(0xFFFFD700);
-  static const Color chatBackground = Color(0xFF0F172A);
-}
-
 // ==============================================================================
 // جهاز التحكم المركزي (Master Navigation Controller)
 // ==============================================================================
