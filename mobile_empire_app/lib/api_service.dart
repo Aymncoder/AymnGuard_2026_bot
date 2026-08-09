09b9e318cdee2cf8cb429353c60bb330f8e138b9
Aymncoder <Aymnalsbyhy2001@gmail.com>
2026-08-09 18:07:58 +0300

    Remove export of app_config.dart
    Removed export statement for app_config.dart.

--- mobile_empire_app/lib/api_service.dart
+++ mobile_empire_app/lib/api_service.dart
@@ -1,8 +1,8 @@
 import 'dart:convert';
 import 'dart:async';
+import 'app_config.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-export 'app_config.dart';
 import 'package:mobile_empire_app/models/bot_model.dart';
 
 // ==============================================================================
