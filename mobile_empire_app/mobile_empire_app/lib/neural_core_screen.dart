07b48954a12f6bcc298a90ff49387d5c536868cc
Aymncoder <Aymnalsbyhy2001@gmail.com>
2026-08-11 06:14:52 +0300

    Update version in Neural Core Screen to v6.0

--- mobile_empire_app/mobile_empire_app/lib/neural_core_screen.dart
+++ mobile_empire_app/mobile_empire_app/lib/neural_core_screen.dart
@@ -1,11 +1,11 @@
 // -*- coding: utf-8 -*-
 /// ==============================================================================
-/// AymnGuard Sovereign Enterprise : Autonomous Neural Core Screen v4.0 (Ultimate)
+/// AymnGuard Sovereign Enterprise : Autonomous Neural Core Screen v6.0 (Absolute)
 /// ==============================================================================
 
 import 'package:flutter/material.dart';
-import 'app_config.dart';
-import 'core/backend_ecosystem.dart';
+import 'package:mobile_empire_app/app_config.dart';
+import 'package:mobile_empire_app/core/backend_ecosystem.dart';
 
 class NeuralCoreScreen extends StatefulWidget {
   const NeuralCoreScreen({super.key});
